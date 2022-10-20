51c6c03716143c52ecc4dbf2ac3ab09a3bff6224
galizar <luisgalicia8@hotmail.com>
2022-10-20 14:19:07 -0400

    haproxy basic benchmark

--- benchmark/native-http.js
+++ benchmark/native-http.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+import http from 'http';
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
 
-require('http').createServer((req, res) => {
+http.createServer((req, res) => {
 	res.end('ok');
 }).listen(PORT, () => {
 	console.log(`Producer running at http://${HOST}:${PORT}`);
