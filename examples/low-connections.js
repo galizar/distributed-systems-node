394439bef6e760871d93aba62c055b4f36f76939
galizar <luisgalicia8@hotmail.com>
2022-10-20 13:37:15 -0400

    native http benchmark

--- examples/low-connections.js
+++ examples/low-connections.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 const server = http.createServer((req, res) => {
 	console.log('current conn', server._connections);
-	setTimeout(() => res.end('OK'), 10_000);
+	setTimeout(() => res.end('OK\n'), 10_000);
 });
 
 server.maxConnections = 2;
