078aa5c01dbbf758cda09683474aa27ab46bae82
galizar <luisgalicia8@hotmail.com>
2022-11-12 23:09:43 -0400

    updated metrics

--- web-api/consumer-http-metrics.js
+++ web-api/consumer-http-metrics.js
@@ -2,29 +2,47 @@
 
 import fastify from 'fastify';
 import fetch from 'node-fetch';
-import StatsD from 'node-statsd';
-import middie from 'middie';
+import StatsD from 'hot-shots';
+import middie from '@fastify/middie';
 
 const server = fastify();
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || 'localhost:4000';
-const statsd = new StatsD({host: 'localhost', port: 8125, prefix: 'web-api'});
+const statsd = new StatsD({host: 'localhost', port: 8125, prefix: 'web-api.'});
 
 (async () => {
 	await server.register(middie);
 
+	// inbound metrics
+	const inboundClient = statsd.childClient({ prefix: '.inbound' });
+
+	// register inbound metrics middleware
+	server.use((req, res, next) => {
+		const startTime = new Date();
+
+		// shadow end request
+		const end = res.end;
+		res.end = () => {
+			res.end = end;
+			end.apply(res);
+
+		}
+
+	});
+	// ---
+
 	// "generic middleware that tracks inbound requests"
-	server.use(statsd.helpers.getExpressMiddleware('inbound', {
-		timeByUrl: true
-	}));
+	// server.use(statsd.helpers.getExpressMiddleware('inbound', {
+		// timeByUrl: true
+	// }));
 
 	server.get('/', async () => {
 		const begin = new Date();
 		const req = await fetch(`http://${TARGET}/recipes/42`);
-		statsd.timing('outbound.recipe-api.request-time', begin);
-		statsd.increment('outbound.recipe-api.request-count');
+		statsd.timing('.outbound.recipe-api.request-time', begin);
+		statsd.increment('.outbound.recipe-api.request-count');
 		const producer_data = await req.json();
 
 		return {
@@ -38,4 +56,4 @@ const statsd = new StatsD({host: 'localhost', port: 8125, prefix: 'web-api'});
 	server.listen({port: PORT, host: HOST}, () => {
 		console.log(`Consumer running at http://${HOST}:${PORT}/`);
 	});
-});
+})();
